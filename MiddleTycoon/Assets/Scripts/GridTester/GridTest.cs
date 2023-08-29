b1aa558e83bd17c3f9615c0b11a53d0cc4816dad
parechae123 <parechae@gmail.com>
2023-08-30 02:18:15 +0900

    그리드시스템 좌표 쇠소값 최대값 오류개선 설치개선

--- MiddleTycoon/Assets/Scripts/GridTester/GridTest.cs
+++ MiddleTycoon/Assets/Scripts/GridTester/GridTest.cs
@@ -1,16 +1,14 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 using static ComponentPipeLine;
 
 public class GridTest : MonoBehaviour
 {
-    public Vector3 savePos;
+    public Vector3 savePos; //마우스 클릭 좌표 위치
     public float gridPivot;
     public Vector2Int[] axisLimit = new Vector2Int[2];//[0] = Min ,[1]=Max
     public HashSet<Vector2Int> gridColl = new HashSet<Vector2Int>();
+    public GameObject installPreview;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,21 +20,36 @@ public class GridTest : MonoBehaviour
         savePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, gridPivot - Camera.main.transform.position.z));
         if (Input.GetMouseButtonDown(0))
         {
-            if (savePos.x >= axisLimit[0].x && savePos.x <= axisLimit[1].x && savePos.y >= axisLimit[0].y && savePos.y <= axisLimit[1].y)
-            {                    
-                Vector2Int gridPos = VectorFTI(new Vector2(savePos.x, savePos.y));
-                GameObject aa = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                if (CollTest(GridMax(LargestMeshFilter(aa).mesh.bounds.min, LargestMeshFilter(aa).mesh.bounds.max, gridPos, false)))
+            if (savePos.x >= axisLimit[0].x && savePos.x <= axisLimit[1].x && savePos.y >= axisLimit[0].y && savePos.y <= axisLimit[1].y&&installPreview != null)
+            {
+                if (CollTest(GridMax(LargestMeshFilter(installPreview).mesh.bounds.min, LargestMeshFilter(installPreview).mesh.bounds.max, VectorFTI(new Vector2(savePos.x, savePos.y)), false)))
                 {
-                    GridMax(LargestMeshFilter(aa).mesh.bounds.min, LargestMeshFilter(aa).mesh.bounds.max, gridPos,true);
-                    aa.transform.position = VectorFTI(savePos);
+                    GridMax(LargestMeshFilter(installPreview).mesh.bounds.min, LargestMeshFilter(installPreview).mesh.bounds.max, VectorFTI(new Vector2(savePos.x, savePos.y)), true);
+                    installPreview.transform.position = VectorFTI(savePos);
+                    installPreview = null;
                 }
                 else
                 {
-                    Destroy(aa);
+                    Destroy(installPreview);
                 }
             }
             //범위 밖이므로 UI메니저에서 추후 에러문 출력필요
+        }
+        if (Input.GetKeyDown(KeyCode.A)&&installPreview == null)
+        {
+            installPreview = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        }
+        if (Input.GetKeyDown(KeyCode.S) && installPreview == null)
+        {
+            installPreview = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        }
+        if (Input.GetKeyDown(KeyCode.D) && installPreview == null)
+        {
+            installPreview = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        }
+        if (installPreview != null&&CollTest(GridMax(LargestMeshFilter(installPreview).mesh.bounds.min, LargestMeshFilter(installPreview).mesh.bounds.max, VectorFTI(new Vector2(savePos.x, savePos.y)), false)))
+        {
+            installPreview.transform.position = VectorFTI(savePos);
         }
     }
 
@@ -48,16 +61,16 @@ public class GridTest : MonoBehaviour
     {
         return new Vector2Int(Mathf.RoundToInt(Mathf.Clamp(orig.x, axisLimit[0].x, axisLimit[1].x)), Mathf.RoundToInt(Mathf.Clamp(orig.y, axisLimit[0].y, axisLimit[1].y)));
     }
-    public List<Vector2Int> GridMax(Vector3 min,Vector3 max,Vector2Int centerPos,bool addCollider)
+    public List<Vector2Int> GridMax(Vector3 min, Vector3 max, Vector2Int centerPos, bool addCollider)
     {
         List<Vector2Int> list = new List<Vector2Int>();
-        list.Add(new Vector2Int(Mathf.FloorToInt(min.x), Mathf.FloorToInt(min.y))+ centerPos);
-        list.Add(new Vector2Int(Mathf.CeilToInt(max.x), Mathf.CeilToInt(max.y))+ centerPos);
+        list.Add(new Vector2Int(Mathf.FloorToInt(min.x), Mathf.FloorToInt(min.y)) + centerPos);
+        list.Add(new Vector2Int(Mathf.CeilToInt(max.x), Mathf.CeilToInt(max.y)) + centerPos);
         for (int i = list[0].x; i <= list[1].x; i++)
         {
             for (int e = list[0].y; e <= list[1].y; e++)
             {
-                if (new Vector2Int(i,e) != list[0] && new Vector2Int(i, e) != list[1])
+                if (new Vector2Int(i, e) != list[0] && new Vector2Int(i, e) != list[1])
                 {
                     if (addCollider)
                     {
@@ -66,16 +79,20 @@ public class GridTest : MonoBehaviour
                     else
                     {
                         list.Add(new Vector2Int(i, e));
-                        Debug.Log(list[list.Count - 1]);
                     }
                 }
             }
+        }
+        if (addCollider)
+        {
+            gridColl.Add(new Vector2Int(Mathf.FloorToInt(min.x), Mathf.FloorToInt(min.y)) + centerPos);
+            gridColl.Add(new Vector2Int(Mathf.CeilToInt(max.x), Mathf.CeilToInt(max.y)) + centerPos);
         }
         return list;
     }
     public bool CollTest(List<Vector2Int> posList)
     {
-        for (int i = 0; i <= posList.Count-1; i++)
+        for (int i = 0; i <= posList.Count - 1; i++)
         {
             if (gridColl.Contains(posList[i]))
             {
